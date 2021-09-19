8bbcd110252a065f76d902e86ed817d988a7693d
AsPulse <aspulse.x@gmail.com>
2021-09-19 14:34:36 +0900

    Add: PWMMotor

--- motor_normal/motor_normal.ino
+++ motor_normal/motor_normal.ino
@@ -1,3 +1,4 @@
+#include <cmath>
 class NormalMotor {
   private: 
     int pin1;
@@ -28,10 +29,65 @@ class NormalMotor {
       digitalWrite(pin2, LOW);
     }
 };
+class PWMMotor {
+  private: 
+    int pin1;
+    int pin2;
+    int pin3;
+    void forward(){
+      digitalWrite(pin1, HIGH);
+      digitalWrite(pin2, LOW);
+    }
+    void backard(){
+      digitalWrite(pin1, LOW);
+      digitalWrite(pin2, HIGH);
+    }
+    void brake(){
+      digitalWrite(pin1, HIGH);
+      digitalWrite(pin2, HIGH);
+    }
+    void off(){
+      digitalWrite(pin1, LOW);
+      digitalWrite(pin2, LOW);
+    }
+    void setPWM(int value) {
+      analogWrite(pin3, value);
+    }
+    int convert255(double value) {
+      return std::round(value * 255);
+    }
+  public:
+    PWMMotor(int sPin1, int sPin2) {
+      pin1 = sPin1;
+      pin2 = sPin2;
+      pin3 = sPin3;
+    }
+    void setup() {
+      pinMode(pin1, OUTPUT);
+      pinMode(pin2, OUTPUT);
+      pinMode(pin3, OUTPUT);
+    }
+    void do(double value){
+      int data = convert255(value);
+      if( data > 255 ) data = 255;
+      if( data < -255 ) data = -255;
+      if( data == 0 ) {
+        off();
+        setPWM(0);
+      }
+      if( data > 0 ) {
+        setPWM(data);
+        forward();
+      }
+      if( data < 0 ) {
+        setPWM(-data)+
+        back();
+      }
+    }
+};
 
-
-NormalMotor leftMotor(13, 12);
-NormalMotor rightMotor(8, 7);
+PWMMotor leftM(8, 7, 10);
+PWMMotor rightM(13, 12, 11);;
 const int delayL = 1000;
 
 void setup() {
@@ -41,8 +97,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  leftMotor.forward();
-  delay(delayL);
-  leftMotor.brake();
-  delay(delayL);
+  leftMotor.do(0.5);
 }
