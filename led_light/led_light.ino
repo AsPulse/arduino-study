18dbf4c209d0d7ab7cd330c82f26b2ad0d586305
AsPulse <aspulse.x@gmail.com>
2021-09-19 12:22:43 +0900

    Add led_light

---
+++ led_light/led_light.ino
@@ -0,0 +1,15 @@
+#define LED_PIN 13
+#define BLINK_DELAY 500
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  delay(BLINK_DELAY);
+  digitalWrite(LED_PIN, LOW);
+  delay(BLINK_DELAY);
+}
